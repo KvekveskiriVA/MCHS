b5ad0616242894f8c51f9c1f99e331dc50e423ea
Vladislav <nerokvent7@gmail.com>
2024-02-28 18:49:21 +0300

    https://pyimagesearch.com/2016/01/04/unifying-picamera-and-cv2-videocapture-into-a-single-class-with-opencv/

---
+++ WebcamVideoStream.py
@@ -0,0 +1,10 @@
+# import the necessary packages
+from threading import Thread
+import cv2
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
